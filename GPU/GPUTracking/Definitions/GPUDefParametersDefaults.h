260584b243ca1d6621891508ea94fbefad301951
Felix Weiglhofer <weiglhofer@fias.uni-frankfurt.de>
2025-06-20 16:27:22 +0200

    GPU: Hack to fix warp wide operations in Zero Suppression decoding.

--- GPU/GPUTracking/Definitions/GPUDefParametersDefaults.h
+++ GPU/GPUTracking/Definitions/GPUDefParametersDefaults.h
@@ -274,7 +274,9 @@
 
   // Default settings for GPU, if not already set for selected GPU type
   #ifndef GPUCA_WARP_SIZE
-    #define GPUCA_WARP_SIZE 32
+    // FIXME: HACK for POCL runtime as warp wide functions seem to broken in
+    // Zero Suppression decoding for values > 2...
+    #define GPUCA_WARP_SIZE 1
   #endif
   #ifndef GPUCA_THREAD_COUNT_DEFAULT
     #define GPUCA_THREAD_COUNT_DEFAULT 256
